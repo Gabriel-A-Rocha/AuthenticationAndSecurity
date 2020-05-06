824220f01961b4b8f243ec3d2c105b2b139ee594
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-05-06 00:29:24 -0300

    Security Level 5 - Cookies and Sessions

--- app.js
+++ app.js
@@ -7,22 +7,41 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 //starting server
 const app = express();
-const port = 3000;
-app.listen(port, () => console.log('Server up and running') );
+app.listen(process.env.PORT, () => console.log('Server up and running'));
 
 //binding required middlewares
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.static('public'));
 
 //define app settings
 app.set('view engine', 'ejs');
 
+//session setup
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: false
+}));
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 //database setup
-mongoose.connect('mongodb://localhost:27017/usersDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/usersDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 //schema creation
 const userSchema = new mongoose.Schema({
@@ -30,73 +49,100 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-//adding encryption to the Schema
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password']
-});
+//inserting plugin to hash and salt registrations
+userSchema.plugin(passportLocalMongoose);
 
 //model to manage a 'users' collection
 const User = mongoose.model('User', userSchema);
 
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 //root route
 app.route('/')
 
-  .get( (req, res) => {
+  .get((req, res) => {
     res.render('home');
   })
 
-  .post( (req, res) => {
+  .post((req, res) => {
     res.send('Hello POST');
   });
 
-//login route
-app.route('/login')
-
-.get( (req, res) => {
-  res.render('login');
-})
-
-.post( (req, res) => {
-  User.findOne({email: req.body.email}, (err, userFound) => {
-    if(err){
-      console.log(err);
-    } else {
-      if(userFound){
-        if(userFound.password === req.body.password){
-          res.render('secrets');
-        }
-      }
-    }
-  });
-});
 
 //register route
 app.route('/register')
 
-.get( (req, res) => {
-  res.render('register');
-})
+  .get((req, res) => {
+    res.render('register');
+  })
 
-.post( (req, res) => {
+  .post((req, res) => {
+    User.register({
+      username: req.body.username
+    }, req.body.password, (err, user) => {
+      if (err) {
+        res.write(err.message);
+        res.redirect('/register');
+      } else {
+        passport.authenticate('local')(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    });
 
-  const newUser = new User({
-    email: req.body.email,
-    password: req.body.password
   });
-  newUser.save( (err) => {
-    if(err){
-      console.log(err);
-    } else {
-      res.render('secrets');
-    }
+
+
+//login route
+app.route('/login')
+  .get((req, res) => {
+    res.render('login');
+  })
+
+  .post((req, res) => {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    req.login(user, function(err){
+      console.log('entrou no login');
+      if(err){
+        console.log(err);
+      } else {
+        passport.authenticate('local')(req, res, function(){
+          res.redirect('/secrets');
+        });
+      }
+    });
+
+
   });
 
-});
+
 
 //submit route
 app.get('/submit', (req, res) => {
   res.render('submit');
+});
+
+
+//secrets route
+app.get('/secrets', (req, res) => {
+  //check if user is logged in before rendering the page
+  if (req.isAuthenticated()) {
+    res.render('secrets');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+//logout route
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
